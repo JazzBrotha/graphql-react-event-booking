690e0ae24513bc5f73afbc04790e324e9532aec4
Mattias <mattiasostblom@protonmail.com>
2019-04-08 17:21:14 +0200

    setup mongodb and mongoose

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
@@ -11,13 +13,28 @@ app.use(
   '/api',
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
 
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(event: EventInput): Event
       }
 
       schema {
@@ -27,11 +44,42 @@ app.use(
 
     `),
     rootValue: {
-      events: () => ['Romantic Cooking', 'Sailing', 'All-Night Coding'],
-      createEvent: ({ name }) => name
+      events: () => {
+        return Event.find()
+          .then(events =>
+            events.map(event => {
+              return { ...event._doc, _id: event.id };
+            })
+          )
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createEvent: ({ event: { title, description, price, date } }) => {
+        const event = new Event({
+          title,
+          description,
+          price: +price,
+          date: new Date().toISOString()
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: event.id };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(`mongodb://localhost/${process.env.MONDO_DB}`)
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err));
